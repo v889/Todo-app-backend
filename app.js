62755a8bc03b81aafb17898191f58090b39033ad
Vishal Gupta <72309826+v889@users.noreply.github.com>
2023-04-25 17:37:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,22 @@ import taskRouter from "./routes/task.js";
 import router from "./routes/user.js";
 import {config} from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 
 export const app=express();
 app.use(express.json())
 app.use(cookieParser())
+app.use(cors({
+    origin:[process.env.FRONTEND_URL],
+    method:["GET","DELETE","PUT","POST"],
+    credentials:true,
+}));
 
 //using rotes
 app.use("/api/v1/users",router);
 app.use("/api/v1/task",taskRouter);
+
 config(
     {
         path:"./data/config.env"
